5b08dde018508cb6955b2a73e06ca61717a42951
Liam Marshall <limarshall@wisc.edu>
2019-08-15 17:06:07 -0500

    max31855: refactor to use boost::units, packed struct, signed bitshifts

--- src/drivers/max31855.cpp
+++ src/drivers/max31855.cpp
@@ -1,11 +1,11 @@
+#include <array>
+#include <boost/units/systems/temperature/celsius.hpp>
+
 #include "max31855.h"
 #include "util/impl_bit_cast.h"
 
-struct Max31855RawReading {
-    int16_t thermocouple;
-    int16_t ref_junction;
-    bool any_fault, scv_fault, scg_fault, oc_fault;
-};
+using namespace boost::units;
+using namespace boost::units::si;
 
 void Max31855::start() {
 #ifdef MAX31855_SHARED
@@ -16,34 +16,36 @@ void Max31855::start() {
     spiReleaseBus(&this->m_spi_driver);
 #endif
 
-    this->state = Max31855State::READY;
+    this->state = Max31855State::Ready;
 }
 
-uint32_t Max31855::spi_read() {
+Max31855RawReading Max31855::raw_spi_read() {
     spiSelect(&this->m_spi_driver);
-    uint8_t data[4];
-    spiReceive(&this->m_spi_driver, 4, data);
+    std::array<uint8_t, 4> data = {};
+    spiReceive(&this->m_spi_driver, 4, data.data());
     spiUnselect(&this->m_spi_driver);
 
-    return bit_cast<uint32_t>(data);
+    return bit_cast<Max31855RawReading>(data);
 }
 
-// struct __attribute__((packed)) reading {
-//     int32_t thermocouple : 12,
+quantity<temperature, float> demangle_temp_thermocouple(uint16_t thermocouple_temp) {
+    // thermocouple_temp is a 14-bit 2's complement signed integer, packed into a uint16_t.
+    // to get a int16_t out of this, we'll shift it left two bits to get the sign bit where
+    // it should be, and then divide by 1<<2 = 4 to restore the magnitude.
+    auto signed_temp = bit_cast<int16_t>((uint16_t)(thermocouple_temp<<2)) / 4;
 
+    // we'll now turn it into a float and wrap it with proper units;
+    // the raw thermocouple temp is given in units of 0.25°C.
+    return signed_temp*0.25*celsius::degrees;
+}
 
-// }
+quantity<temperature, float> demangle_temp_internal(uint16_t internal_temp) {
+    // internal_temp is a 12-bit 2's complement signed integer, packed into a uint16_t.
+    // to get a int16_t out of this, we'll shift it left four bits to get the sign bit where
+    // it should be, and then divide by 1<<4 = 16 to restore the magnitude.
+    auto signed_temp = bit_cast<int16_t>((uint16_t)(internal_temp<<4)) / 16;
 
-Max31855RawReading parse_reading(uint32_t data) {
-    Max31855RawReading reading = {
-        .thermocouple = 0,
-        .ref_junction = 0,
-
-        .any_fault = data & (1 << 16),
-        .scv_fault = data & (1 << 2),
-        .scg_fault = data & (1 << 1),
-        .oc_fault = data & (1 << 0),
-    };
-
-    return reading;
+    // we'll now turn it into a float and wrap it with proper units;
+    // the internal thermocouple temp is given in units of 0.0625°C.
+    return signed_temp*0.0625*celsius::degrees;
 }
--- src/drivers/max31855.h
+++ src/drivers/max31855.h
@@ -5,22 +5,34 @@
 #define MAX31855_SHARED TRUE
 
 enum class Max31855State {
-    UNINIT,
-    STOPPED,
-    READY,
+    Uninit,
+    Stopped,
+    Ready,
 };
+
+struct __attribute__((packed)) Max31855RawReading {
+    uint16_t thermocouple_temp : 14;
+    /* ==== reserved ==== */ uint8_t _reserved1 : 1;
+    bool fault_any : 1;
+    uint16_t internal_temp : 12;
+    /* ==== reserved ==== */ uint8_t _reserved2 : 1;
+    bool fault_short_vcc : 1;
+    bool fault_short_gnc : 1;
+    bool fault_open : 1;
+};
+static_assert(sizeof(Max31855RawReading) == 4, "Max31855RawReading didn't pack to a uint32_t");
 
 class Max31855 {
 public:
     Max31855(SPIDriver &spi_driver, SPIConfig &spi_config)
         : m_spi_driver(spi_driver),
           m_spi_config(spi_config),
-          state(Max31855State::UNINIT) {}
+          state(Max31855State::Uninit) {}
 
     void start();
 
 private:
-    uint32_t spi_read();
+    Max31855RawReading raw_spi_read();
 
     SPIDriver &m_spi_driver;
     SPIConfig &m_spi_config;
