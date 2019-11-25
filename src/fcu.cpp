ce2c56c2ef6dfdfe631a0aae0635345f463a38bc
Liam Marshall <limarshall@wisc.edu>
2019-11-24 19:16:35 -0600

    fcu: remove max31855 test code

--- src/fcu.cpp
+++ src/fcu.cpp
@@ -5,7 +5,6 @@
 #include "hal.h"
 #include "sys/fault/handlers.h"
 
-#include "drivers/max31855.h"
 #include "filesystem.h"
 
 using namespace chibios_rt;
@@ -24,48 +23,6 @@ static WDGConfig wdg_config = {
     .rlr = STM32_IWDG_RL(1000),
 };
 
-
-
-static SPIConfig spicfg = {
-    .circular = false,
-    .end_cb = NULL,
-    .ssline = LINE_SPI1_CS,
-    .cr1 = SPI_CR1_BR_2 | SPI_CR1_BR_1,
-    .cr2 = 0,
-};
-
-class ThermocoupleThread : public BaseStaticThread<1024> {
-public:
-    ThermocoupleThread(Max31855 tcouple) : m_tcouple(std::move(tcouple)) {}
-protected:
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wmissing-noreturn"
-    void main() override {
-        setName("tcouple");
-
-        m_tcouple.start();
-        while (true) {
-            auto reading = m_tcouple.read();
-            printf("faults: any short2vcc short2gnd open\n        %d   %d        %d       %d\n",
-                reading.fault_any(), reading.fault_short_vcc(), reading.fault_short_gnd(), reading.fault_open());
-            auto itemp = reading.internal_temp();
-            auto ttemp = reading.thermocouple_temp();
-
-            printf("internal temp is: %f\n", itemp.value());
-            printf("thermocouple temp is: %f\n", ttemp.value());
-
-            palToggleLine(LINE_LED2);
-            chThdSleepMilliseconds(200);
-        }
-    }
-#pragma clang diagnostic pop
-
-private:
-    Max31855 m_tcouple;
-};
-
-
-
 class SDThread : public BaseStaticThread<4096> {
 public:
     SDThread() {}
@@ -81,8 +38,6 @@ private:
     FilesystemComponent m_fs;
 };
 
-static ThermocoupleThread thd_tcouple(
-    std::move(Max31855(SPID1, spicfg)));
 static SDThread thd_sd;
 
 int main() {
