bc9063375eb85d4242cbb48c7b38136bb3961ad6
Liam Marshall <limarshall@wisc.edu>
2019-12-20 22:44:16 -0600

    imu: drop spi clockspeed

--- src/fcu.cpp
+++ src/fcu.cpp
@@ -28,7 +28,7 @@ static SPIConfig spicfg_imu = {
     .circular = false,
     .end_cb = NULL,
     .ssline = LINE_SPI3_CS,
-    .cr1 = SPI_CR1_BR_1 | SPI_CR1_CPOL | SPI_CR1_CPHA,
+    .cr1 = SPI_CR1_BR_0 | SPI_CR1_BR_1 | SPI_CR1_BR_2 | SPI_CR1_CPOL | SPI_CR1_CPHA,
     .cr2 = 0,
 };
 
