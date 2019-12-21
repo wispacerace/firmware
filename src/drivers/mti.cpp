de17942b6a35d13b4c73822caa1185a03e779fba
Liam Marshall <limarshall@wisc.edu>
2019-12-20 22:46:03 -0600

    mti: necessary functions to wake up, configure, and measure

--- src/drivers/mti.cpp
+++ src/drivers/mti.cpp
@@ -1,5 +1,55 @@
 #include <cstdio>
+#include <cassert>
 #include "mti.h"
+
+void MtiIMU::handle_data(IMUSyncPipe *sync) {
+    spiSelect(&this->m_spi_driver);
+    this->send_opcode(XBUS_PIPE_STATUS);
+
+    mtssp_status status = this->read_status();
+    spiUnselect(&this->m_spi_driver);
+
+
+    if (status.notification_message_size > 0) {
+        // printf("processing notifications\n");
+        this->read_data(XBUS_NOTIFICATION_PIPE, status.notification_message_size, sync);
+    } else if (status.measurement_message_size > 0) {
+        // printf("processing measurements\n");
+        this->read_data(XBUS_MEASUREMENT_PIPE,  status.measurement_message_size, sync);
+    }
+}
+
+void MtiIMU::read_data(uint8_t pipe, uint16_t length, IMUSyncPipe *sync) {
+    assert(length <= max_xbus_message_size);
+
+    spiSelect(&this->m_spi_driver);
+    this->send_opcode(pipe); // select the given pipe
+    spiReceive(&this->m_spi_driver, length, this->m_message_buffer); // and drain (length) bytes out of it
+    spiUnselect(&this->m_spi_driver);
+
+
+    // grab a message
+    auto idx = 0;
+    while (idx < length) {
+        auto msg = (XbusParsedMessage*) chPoolAlloc(&sync->msgs_pool);
+        if (msg != nullptr) {
+            auto parsed_length = xbus_buf2msg(msg, this->m_message_buffer + idx, length - idx);
+            if (parsed_length != -1) {
+                idx += parsed_length;
+                // printf("!\n");
+
+                if (msg->id == XMID_MtData2) {
+                    chMBPostTimeout(&sync->datamsg_mbox, (msg_t)msg, TIME_IMMEDIATE);
+                } else {
+                    chMBPostTimeout(&sync->auxmsg_mbox, (msg_t)msg, TIME_IMMEDIATE);
+                }
+            } else {
+                chPoolFree(&sync->msgs_pool, msg);
+                break;
+            }
+        }
+    }
+}
 
 void MtiIMU::start() {
     printf("[mti]: starting spi... ");
@@ -9,4 +59,121 @@ void MtiIMU::start() {
     palEnableLineEvent(this->m_line_DRDY, PAL_EVENT_MODE_RISING_EDGE);
 }
 
+bool MtiIMU::wakeup(IMUSyncPipe *sync) {
+    // bounce the reset line to make sure the IMU is in a clean state
+    palSetLine(this->m_line_RST);
+    chThdSleepMilliseconds(10);
+    palClearLine(this->m_line_RST); // release reset line
+
+    printf("[mti]: reset... waiting for wakeup packet\n");
+    if (wait_for_wakeup(TIME_MS2I(1000), sync)) {
+        printf("[mti]: sending wakeup ack... ");
+
+        XbusMessage msg = {.mid = XMID_WakeupAck};
+        this->send_message(&msg);
+        printf("ok\n");
+    }
+
+    return false;
+}
+
+uint32_t MtiIMU::read_device_id(IMUSyncPipe *sync) {
+    XbusMessage tx = {.mid = XMID_ReqDid};
+    auto rx = this->transact_message(&tx, sync);
+
+    if (rx != nullptr) {
+        if (rx->id == XMID_DeviceId) {
+            uint32_t id = (uint32_t)rx->payload[0] << 24
+                        | (uint32_t)rx->payload[1] << 16
+                        | (uint32_t)rx->payload[2] << 8
+                        | (uint32_t)rx->payload[3] << 0;
+            chPoolFree(&sync->msgs_pool, rx);
+            return id;
+        }
+        chPoolFree(&sync->msgs_pool, rx);
+    }
+
+    return 0;
+}
+
+void MtiIMU::set_output_config(tcb::span<mti::OutputConfigPair> cfg) {
+    uint8_t data[32]; // TODO: should we be bounds checking this?
+
+    auto payload_length = mti::format_output_config(data, cfg);
+    XbusMessage msg = {.mid = XMID_SetOutputConfig, .length = (uint16_t) payload_length, .data = data};
+    this->send_message(&msg);
+}
+
+void MtiIMU::go_to_state(mti::DeviceState state) {
+    XbusMessage msg = {};
+    switch (state) {
+        case mti::DeviceState::Configuration: msg.mid = XMID_GotoConfig; break;
+        case mti::DeviceState::Measurement: msg.mid = XMID_GotoMeasurement; break;
+    }
+
+    this->send_message(&msg);
+}
+
+
+void MtiIMU::send_opcode(uint8_t opcode) {
+    uint8_t data[4] = {opcode, 0x0, 0x0, 0x0};
+    spiSend(&this->m_spi_driver, sizeof(data), data);
+}
+
+mtssp_status MtiIMU::read_status() {
+    uint8_t status_data[4];
+    spiReceive(&this->m_spi_driver, sizeof(status_data), status_data);
+
+    mtssp_status status = {
+        .notification_message_size = (uint16_t)(status_data[0] | (status_data[1] << 8)),
+        .measurement_message_size  = (uint16_t)(status_data[2] | (status_data[3] << 8)),
+    };
+
+    return status;
+}
+
+void MtiIMU::wait_drdy() {
+    palWaitLineTimeout(this->m_line_DRDY, TIME_INFINITE);   
+}
+
+bool MtiIMU::wait_for_wakeup(sysinterval_t timeout, IMUSyncPipe *sync) {
+    XbusParsedMessage *msg;
+    auto res = chMBFetchTimeout(&sync->auxmsg_mbox, (msg_t*)&msg, timeout);
+    if (res == MSG_OK) {
+        auto id = msg->id;
+        chPoolFree(&sync->msgs_pool, msg);
+        if (id == XMID_Wakeup) {
+            printf("[mti]: got wakeup packet\n");
+            return true;
+        }
+    }
+    return false;
+}
+
+void MtiIMU::send_message(XbusMessage *msg) {
+    uint8_t buf[64]; // TODO: should we be bounds checking this?
+    size_t length = XbusMessage_format(buf, msg, XLLF_Spi);
+    // __asm__("bkpt");
+    spiSelect(&this->m_spi_driver);
+    spiSend(&this->m_spi_driver, length, buf);
+    spiUnselect(&this->m_spi_driver);
+}
+
+XbusParsedMessage* MtiIMU::transact_message(XbusMessage *tx, IMUSyncPipe *sync) {
+    this->send_message(tx);
+
+    XbusParsedMessage *rx;
+    auto res = chMBFetchTimeout(&sync->auxmsg_mbox, (msg_t*)&rx, TIME_MS2I(500));
+    return (res == MSG_OK) ? rx : nullptr;
+}
+
+size_t mti::format_output_config(uint8_t *buf, tcb::span<OutputConfigPair> cfg) {
+        size_t idx = 0;
+        for (const auto& pair : cfg) {
+            buf[idx++] = (pair.data_id >> 8)   & 0xff;
+            buf[idx++] = pair.data_id          & 0xff;
+            buf[idx++] = (pair.frequency >> 8) & 0xff;
+            buf[idx++] = pair.frequency        & 0xff;
+        }
+        return idx;
 }
--- src/drivers/mti.h
+++ src/drivers/mti.h
@@ -6,54 +6,78 @@
 #include <xbusmessage.h>
 #include <xbusdef.h>
 #include <utility>
+#include <span.h>
+
+#include "drivers/xbus/parser.h"
 
 using namespace chibios_rt;
 
-
 constexpr auto max_xbus_message_size = 128;
-constexpr auto xbus_mq_size = 4; // # message deep queue between ISR and main functions
-
-class MtiIMU;
-
-class IMUMessageHandler : public BaseStaticThread<0xff> {
-public:
-    IMUMessageHandler(MtiIMU *imu)
-        : m_imu(imu) {}
-protected:
-    void main() override;
-
-private:
-    MtiIMU *m_imu;
-};
 
 struct mtssp_status {
     uint16_t notification_message_size;
     uint16_t measurement_message_size;
 };
 
+
+namespace mti {
+    struct OutputConfigPair {
+        OutputConfigPair(uint16_t data_id, uint16_t frequency) : data_id(data_id), frequency(frequency) {}
+        uint16_t data_id;
+        uint16_t frequency;
+    };
+
+    size_t format_output_config(uint8_t *buf, tcb::span<OutputConfigPair> cfg);
+    // TODO: unit test!
+
+    enum class DeviceState {
+        Configuration,
+        Measurement
+    };
+}
+
+
+constexpr auto message_pool_size = 4;
+constexpr auto data_queue_size = 2;
+constexpr auto aux_queue_size = 1;
+struct IMUSyncPipe {
+    IMUSyncPipe() {
+        chMBObjectInit(&this->datamsg_mbox, this->datamsg_queue, data_queue_size);
+        chMBObjectInit(&this->auxmsg_mbox, this->auxmsg_queue, aux_queue_size);
+        chPoolObjectInit(&this->msgs_pool, /* entry size = */ sizeof(XbusParsedMessage), nullptr);
+        chPoolLoadArray(&this->msgs_pool, this->msgs, message_pool_size);
+    }
+
+    mailbox_t datamsg_mbox;
+    mailbox_t auxmsg_mbox;
+    msg_t datamsg_queue[data_queue_size] = {};
+    msg_t auxmsg_queue[aux_queue_size] = {};
+
+    uint8_t msgs[max_xbus_message_size][message_pool_size];
+    memory_pool_t msgs_pool;
+};
+
 // NOTE: this must be the only thing on the given SPIDriver
 // unlike max31855, we make no effort to restart or acquire the bus,
 // since sharing the IMU SPI bus with other devices would be a Bad Idea™
 class MtiIMU {
-    friend IMUMessageHandler;
 public:
     MtiIMU(SPIDriver &spi_driver, SPIConfig &spi_config,
            ioline_t line_DRDY, ioline_t line_RST)
         : m_spi_driver(spi_driver),
           m_spi_config(spi_config),
           m_line_DRDY(line_DRDY),
-          m_line_RST(line_RST),
-
-          m_thd_handler(this)
-    {
-        chPoolObjectInit(&this->m_msgbuf_mempool, /* entry size = */ max_xbus_message_size, nullptr);
-        chPoolLoadArray(&this->m_msgbuf_mempool, this->m_message_buffers, xbus_mq_size);
-    }
+          m_line_RST(line_RST)
+    {}
 
     void start();
-    bool wakeup();
+    bool wakeup(IMUSyncPipe *sync);
+    uint32_t read_device_id(IMUSyncPipe *sync);
+    void set_output_config(tcb::span<mti::OutputConfigPair> cfg);
+    void go_to_state(mti::DeviceState state);
 
-
+    void wait_drdy();
+    void handle_data(IMUSyncPipe *sync);
 private:
     SPIDriver &m_spi_driver;
     SPIConfig &m_spi_config;
@@ -61,14 +85,14 @@ private:
     ioline_t m_line_DRDY;
     ioline_t m_line_RST;
 
-    uint8_t m_message_buffers[xbus_mq_size][max_xbus_message_size];
-    memory_pool_t m_msgbuf_mempool;
-
-    IMUMessageHandler m_thd_handler;
-
-    void cb_read_message();
+    uint8_t m_message_buffer[max_xbus_message_size];
 
     void send_opcode(uint8_t opcode);
     mtssp_status read_status();
-    void read_data(uint8_t pipe, uint16_t length);
+    void read_data(uint8_t pipe, uint16_t length, IMUSyncPipe *sync);
+
+    void send_message(XbusMessage *msg);
+    XbusParsedMessage* transact_message(XbusMessage *msg, IMUSyncPipe *sync);
+
+    bool wait_for_wakeup(sysinterval_t timeout, IMUSyncPipe *sync);
 };
