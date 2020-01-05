4005539975856af76f6e89217a9bc184e21e48ef
Liam Marshall <limarshall@wisc.edu>
2020-01-04 18:26:07 -0600

    drivers/servo: fix MISRA violations

--- src/drivers/servo.cpp
+++ src/drivers/servo.cpp
@@ -2,27 +2,21 @@
 #include "ch.h"
 #include "hal.h"
 
-pwmcnt_t degreesToTicks(PWMDriver *driver, int angle) {
+pwmcnt_t degreesToTicks(const PWMDriver *driver, int angle) {
     /* 180 is degrees spread across 900 to 2100 microseconds */
     return (PWM_FRACTION_TO_WIDTH(driver, 20000, 900) +
             PWM_FRACTION_TO_WIDTH(driver, 20000, angle * 1200 / 180));
 }
 
-Servo::Servo(PWMDriver *drv, pwmchannel_t chnl) {
-    this->channel = chnl;
-    this->drv = drv;
-    this->angle = 90;
-}
-
-void Servo::write(int angle) {
+void Servo::write(int new_angle) {
     pwmcnt_t width;
 
-    if (angle < 0)
-        angle = 0;
-    else if (angle > 180)
-        angle = 180;
-    this->angle = angle;
-    width = degreesToTicks(this->drv, angle);
+    if (new_angle < 0)
+        new_angle = 0;
+    else if (new_angle > 180)
+        new_angle = 180;
+    this->angle = new_angle;
+    width = degreesToTicks(this->drv, this->angle);
     pwmEnableChannel(this->drv, this->channel, width);
 }
 
